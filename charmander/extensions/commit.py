464b2fa8e3c74113a0f7a9a98f030e85fade8231
vicky002 <tvicky002@gmail.com>
2016-01-10 06:57:29 +0530

    get random commit messages

---
+++ charmander/extensions/commit.py
@@ -0,0 +1,24 @@
+__author__ = 'vikesh'
+
+# Get random commit messages
+""" commit returns random commit messages"""
+
+import re
+import requests
+
+
+def commit():
+    return requests.get("http://whatthecommit.com/index.txt").text
+
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"commit( .*)?", text)
+
+    if not match:
+        return
+
+    return commit()
+
+
+
